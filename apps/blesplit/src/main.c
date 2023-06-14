apache/mynewt-core

baselibc: Call mynewt_main() instead of main()

apps/blesplit/src/main.c
@@ -284,7 +284,7 @@ blesplit_on_sync(void)
  * @return int NOTE: this function should never return!
  */
 int
-main(void)
+mynewt_main(int argv, char **argv)
 {
     int rc;
 

apps/bsncent/src/main.c
@@ -411,7 +411,7 @@ bsncent_on_sync(void)
  * @return int NOTE: this function should never return!
  */
 int
-main(void)
+mynewt_main(int argv, char **argv)
 {
     int rc;
 

apps/bsnprph/src/main.c
@@ -318,7 +318,7 @@ bsnprph_on_sync(void)
  * @return int NOTE: this function should never return!
  */
 int
-main(void)
+mynewt_main(int argv, char **argv)
 {
     int rc;
 

apps/crypto_test/src/main.c
@@ -933,7 +933,7 @@ run_iovec_test(struct crypto_dev *crypto)
 #endif /* MYNEWT_VAL(CRYPTOTEST_IOVEC) */
 
 int
-main(void)
+mynewt_main(int argv, char **argv)
 {
     struct crypto_dev *crypto;
 #if MYNEWT_VAL(CRYPTOTEST_BENCHMARK)

apps/hash_test/src/main.c
@@ -580,7 +580,7 @@ run_concurrency_test(struct hash_dev *hash)
 }
 
 int
-main(void)
+mynewt_main(int argv, char **argv)
 {
     struct hash_dev *hash;
     struct hash_sha256_context hash_sha256;

apps/lora_app_shell/src/main.c
@@ -135,7 +135,7 @@ static const oc_handler_t omgr_oc_handler = {
 };
 
 int
-main(void)
+mynewt_main(int argv, char **argv)
 {
 #ifdef ARCH_sim
     mcu_sim_parse_args(argc, argv);

apps/loraping/src/main.c
@@ -213,7 +213,7 @@ on_rx_error(void)
 }
 
 int
-main(void)
+mynewt_main(int argv, char **argv)
 {
     RadioEvents_t radio_events;
 

kasjer/incubator-mynewt-core

apps/blesplit/src/main.c
@@ -284,7 +284,7 @@ blesplit_on_sync(void)
  * @return int NOTE: this function should never return!
  */
 int
-main(void)
+mynewt_main(int argc, char **argv)
 {
     int rc;
 

apps/bsncent/src/main.c
@@ -411,7 +411,7 @@ bsncent_on_sync(void)
  * @return int NOTE: this function should never return!
  */
 int
-main(void)
+mynewt_main(int argc, char **argv)
 {
     int rc;
 

apps/bsnprph/src/main.c
@@ -318,7 +318,7 @@ bsnprph_on_sync(void)
  * @return int NOTE: this function should never return!
  */
 int
-main(void)
+mynewt_main(int argc, char **argv)
 {
     int rc;
 

apps/crypto_test/src/main.c
@@ -933,7 +933,7 @@ run_iovec_test(struct crypto_dev *crypto)
 #endif /* MYNEWT_VAL(CRYPTOTEST_IOVEC) */
 
 int
-main(void)
+mynewt_main(int argc, char **argv)
 {
     struct crypto_dev *crypto;
 #if MYNEWT_VAL(CRYPTOTEST_BENCHMARK)

apps/hash_test/src/main.c
@@ -580,7 +580,7 @@ run_concurrency_test(struct hash_dev *hash)
 }
 
 int
-main(void)
+mynewt_main(int argc, char **argv)
 {
     struct hash_dev *hash;
     struct hash_sha256_context hash_sha256;

apps/lora_app_shell/src/main.c
@@ -135,7 +135,7 @@ static const oc_handler_t omgr_oc_handler = {
 };
 
 int
-main(void)
+mynewt_main(int argc, char **argv)
 {
 #ifdef ARCH_sim
     mcu_sim_parse_args(argc, argv);

apps/loraping/src/main.c
@@ -213,7 +213,7 @@ on_rx_error(void)
 }
 
 int
-main(void)
+mynewt_main(int argc, char **argv)
 {
     RadioEvents_t radio_events;
 